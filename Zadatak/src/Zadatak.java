47ee1dc61d03371a00b1b146a82a96d05af92e26
dejanadamjanovic <dejanadamjanovic17@gmail.com>
2026-06-29 11:09:14 +0200

    Obrisan komentar

--- Zadatak/src/Zadatak.java
+++ Zadatak/src/Zadatak.java
@@ -1,13 +1,6 @@
 
 public class Zadatak {
 	
-	/*
-	 * Racuna sumu kvadrata prvih n elemenata
-	 * 
-	 * n treba da bude pozitivni broj;
-	 * 
-	 *
-	 */
 	
 	public int sumaKvadrata(int n) {
 		int suma = 0;
